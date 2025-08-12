4c9c724e3e114479ddc166a145822cea204411c9
woodsbran <164268290+woodsbran@users.noreply.github.com>
2025-08-12 12:47:23 -0700

    updated MainActivity so that when a user logs in, their email is sent to the next screen using intent.putExtra("userEmail", user.getEmail()). This makes sure SettingsPage and EditProfilePage know which user is logged in, so they can pull the correct data from the database and pre-fill the profile fields. Before this, the email wasn’t being passed, so EditProfilePage couldn’t load the user’s info.

--- app/src/main/java/com/example/classroomannouncement/IUserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.classroomannouncement;
-
-import com.example.classroomannouncement.Database.Entities.User;
-
-// Interface for mocking UserRepo behavior in tests
-public interface IUserRepo {
-    User getUserByEmail(String email);
-}
